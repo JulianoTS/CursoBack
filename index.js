520f087966be253e95bb82649e0434db8a81497c
JULIANO TAVARES DA SILVA <juliano_t_da-silva@estudante.sesisenai.org.br>
2024-07-04 16:28:27 -0300

    second commit

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
-function filtrarPares(arr){
+/*function filtrarPares(arr){
     nova_arr = []
     for(let i = 0; i < arr.length; i++){
         if(arr[i] % 2 === 0){
             nova_arr.push(arr[i])
         }
     }
-
     return nova_arr
 }
 
-console.log(filtrarPares([1, 2, 3, 4, 5, 6]))
+console.log(filtrarPares([1, 2, 3, 4, 5, 6]))*/
+
+const express = require('express')
+
+const app = express()
+
+app.get('/test-api', function(req, res) {
+    res.send(' NOSSA API TA FUNCIONANDO')
+})
+
+app.listen(8000)
