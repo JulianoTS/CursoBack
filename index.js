3bd501751671d8e9de78cd4bc170eae54c6e5b6a
JULIANO TAVARES DA SILVA <juliano_t_da-silva@estudante.sesisenai.org.br>
2024-07-04 15:21:00 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+function filtrarPares(arr){
+    nova_arr = []
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i] % 2 === 0){
+            nova_arr.push(arr[i])
+        }
+    }
+
+    return nova_arr
+}
+
+console.log(filtrarPares([1, 2, 3, 4, 5, 6]))
